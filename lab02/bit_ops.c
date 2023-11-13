07950725001ff20f09994acb98ae5f557d274caa
Kidulter <1909880919@qq.com>
2023-11-13 14:55:59 +0000

    finished lab02/bit_ops

---
+++ lab02/bit_ops.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "bit_ops.h"
+
+// Return the nth bit of x.
+// Assume 0 <= n <= 31
+unsigned get_bit(unsigned x,
+                 unsigned n) {
+    // YOUR CODE HERE
+    // Returning -1 is a placeholder (it makes
+    // no sense, because get_bit only returns 
+    // 0 or 1)
+    return (x >> n) & 0x1;
+}
+// Set the nth bit of the value of x to v.
+// Assume 0 <= n <= 31, and v is 0 or 1
+void set_bit(unsigned * x,
+             unsigned n,
+             unsigned v) {
+    // YOUR CODE HERE
+    /* if (v==1) {
+        (*x) = (*x) | (v << n);   // *x | 100000000...
+    }else {
+        (*x) = (*x) & (~((v+1) << n));   // *x & 011111111...
+    } */
+    (*x) = (*x) & (~(1 << n));
+    (*x) = (*x) | (v << n);
+    // use "&v", "& (~v)" to make another operation generate (if target op is 1, another is 0) "0x0"
+    // then to do the "or" operation with target op, so this "| 0x0" does nothing to the aim value
+    // and the target op will "& 0x1" will not change any bit.
+}
+// Flip the nth bit of the value of x.
+// Assume 0 <= n <= 31
+void flip_bit(unsigned * x,
+              unsigned n) {
+    // YOUR CODE HERE
+    unsigned nth = ((*x) >> n) & 0x1;
+    int v = nth ^ 1;
+    set_bit(x, n, v);
+}
+
