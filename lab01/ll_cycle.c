ac7f7d42db31535d8b471247fe35bc586f7b860b
Kidulter <1909880919@qq.com>
2023-11-13 08:33:50 +0000

    code has segmentation fault(core dump)

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,33 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node* t1 = head;
+    node* h2 = head;
+    if (h2->next != NULL){
+	  h2 = h2->next;
+	  if (h2->next != NULL){
+	        h2 = h2->next;
+	  }else{
+	        return 0;
+	  }
+    }else{
+          return 0;
+    }
+    t1 = t1->next;
+    while (t1 != h2) {
+     if (h2->next != NULL){
+	  h2 = h2->next;
+	  if (h2->next != NULL){
+	        h2 = h2->next;
+	  }else{
+	        return 0;
+	  }
+    }else{
+          return 0;
+    }
+    t1 = t1->next;
+     if (t1 == h2){
+         return 1;
+     }
+    }
 }
